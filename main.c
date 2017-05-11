a736822f9b5ce7677381370f7f64adbb84bc44d4
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-11 10:52:31 +0700

    Added incput

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-void input(char *array)
+void input(char *arr)
 {
-array[2] = '2';
-
-
+int i;
+    printf("Введите путь (в конце пробел)\n");
+    for (i = 0;arr[i - 1] != ' ' ; i++){
+        arr[i] = getchar();
+    }
 }
 
 int main()
 {
-char value[5] = {}, k;
-
+char value[50] = {}, k;
 input(value);
-printf("%c\n", value[2]);
+printf("str:%c %c \n", value[2], value[3] );
     return 0;
 }
