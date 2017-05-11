153b5a03bbdea50d0a7a46dff27278e748f46ed5
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-11 12:38:12 +0700

    Check for valid characters

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define MAX_PATH 260
 
 void input(char *arr)
 {
@@ -7,12 +8,40 @@ int i;
     for (i = 0;arr[i - 1] != ' ' ; i++){
         arr[i] = getchar();
     }
+    arr[i - 1] = '0';
+}
+
+void check(char *arr, int *rez)
+{
+int i, j;
+char forbiddenSymbol[9] = {'/', ':', '*', '?', 92, 34, '<', '>', '|'};
+
+    for (i = 260; i < 270; i++){
+        if (arr[i] != 0){
+            rez[0] = 1;
+        }
+    }
+    for (i = 0; i < 260; i++){
+        for (j = 0; j < 10; j++){
+            if (arr[i] == forbiddenSymbol[j]){
+                rez[0] = 2;
+                rez[1] = i;
+            }
+        }
+    }
+
+
 }
 
 int main()
 {
-char value[50] = {}, k;
-input(value);
-printf("str:%c %c \n", value[2], value[3] );
+char value[270] = {0};
+int k = -5, checkRez[2] = {-5, -5};
+
+    input(value);
+
+    check(value, checkRez);
+    printf("check: %d  %d\n", checkRez[0], checkRez[1]);
+
     return 0;
 }
