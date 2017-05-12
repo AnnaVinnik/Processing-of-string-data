59ca9e5f5854b08861e5709d42b3f13db8ccaaac
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-12 10:57:40 +0700

    Added delete, up, substitution

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int i;
     for (i = 0;arr[i - 1] != ' ' ; i++){
         arr[i] = getchar();
     }
-    arr[i - 1] = '0';
+    arr[i - 1] = ' ';
 }
 
 void check(char *arr, int *rez)
@@ -33,15 +33,56 @@ char forbiddenSymbol[9] = {'/', ':', '*', '?', 92, 34, '<', '>', '|'};
 
 }
 
+void delete(char *arr, int since, int quantity)
+{
+int i, j;
+    for (j = 0; j < quantity; j++){
+        for (i = since; i < 260 - 1; i++){
+            arr[i] = arr[i + 1];
+        }
+    }
+}
+
+void clean(char *arr)
+{
+int i;
+    for (i = 0; i < 260; i++){
+        arr[i] = ' ';
+    }
+}
+
+void print(char *arr)
+{
+    for (int i = 0; i < 10; i++){
+        printf ("%c", arr[i]);
+    }
+    printf("\n");
+}
+
+void up(char *arr, int since)
+{
+    arr[since] -=32;
+}
+
+void substitution(char *arr, char before, char after)
+{
+int i;
+    for (i = 0; i < 260; i++){
+        if (arr[i] == before){
+            arr[i] = after;
+        }
+    }
+}
+
 int main()
 {
 char value[270] = {0};
 int k = -5, checkRez[2] = {-5, -5};
 
     input(value);
+    substitution(value, 92, '/');//  92 = '\'
 
-    check(value, checkRez);
-    printf("check: %d  %d\n", checkRez[0], checkRez[1]);
+    print(value);
 
     return 0;
 }
