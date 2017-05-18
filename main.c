44151e1087f6b3f47b68e8e1a16611171635ef9d
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-18 12:36:06 +0700

    Inicial

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX_PATH 260
 
 void input(char *arr)
@@ -84,6 +85,27 @@ int i, j;
 
 }
 
+int slen(char *str)
+{
+int i, len;
+    for (i = 0; str[i] != '\0'; i++);
+    len = i;
+    return len;
+}
+
+int scmp(char str1[], char str2[])
+{
+    if (slen(str1) != slen(str2))
+        return 0;
+    else for (int i = 0; str1[i] != '\0'; i++){
+        if (str1[i] != str2[i]){
+            return 0;
+        }
+    }
+    return 1;
+
+
+}
 
 void add(char *arr, int since, char symbol)
 {
@@ -91,18 +113,47 @@ void add(char *arr, int since, char symbol)
    arr[since] = symbol;
 }
 
+int schr(char str[], char delim)
+{
+int i;
+    for (i = 0; str[i] != delim;)
+        if (str[i] != '\0')
+            i++;
+        else return -1;
+    return i;
+}
+
+int stok(char str[], char delim, char *ptr[])
+{
+char *suf = str;
+ptr[0] = str; // первое поле – начало str
+int i, j = 1; // j – счетчик полей
+
+        for(; (i = schr(suf, delim)) >= 0 ;){
+            suf[i] = '\0';
+            suf = suf + i + 1;
+            ptr[j] = suf;
+            j++;
+        }
+printf("j in stock %d \n", j);
+    
+return j;
+}
+
 int main()
 {
 char value[270] = {0};
-int k = -5, checkRez[2] = {-5, -5};
-
+char **ptr, delim = '/', cygdrive[] = "cygdrive";
+int k = -5, checkRez[2] = {-5, -5}, len, i, scm;
+   
+    len = slen(value);
     input(value);
-    delete(value, 0, 9);
-    up(value, 0);
-    add(value, 1, ':');
-    substitution(value, '/', 92);//  92 = '\'
-    printf("Новый путь:");
-    print(value);
+    int j = stok(value, delim, ptr);
+    //for (i = 0; i < j; i++);
+    if ((scmp(ptr[0], cygdrive)) == 1){
+        //ptr[1][0] = ptr[1][0] - 32;
+        printf("%c\n", value[3]);
+    }
 
     return 0;
 }
